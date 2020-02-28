dd5944766ca47269434f3abbce4ca254a8a132aa
alex sarnese <alexthepokemon@Gmail.com>
2020-02-27 23:52:17 -0500

    portal blocks looks great

--- src/main/java/net/telepathicgrunt/worldblender/blocks/WBPortalBlock.java
+++ src/main/java/net/telepathicgrunt/worldblender/blocks/WBPortalBlock.java
@@ -11,20 +11,23 @@ import net.minecraft.entity.Entity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.tileentity.EndGatewayTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.telepathicgrunt.worldblender.dimension.WBDimension;
 
 
 public class WBPortalBlock extends ContainerBlock
 {
 	protected WBPortalBlock()
 	{
-		super(Block.Properties.create(Material.PORTAL, MaterialColor.BLACK).doesNotBlockMovement().lightValue(5).hardnessAndResistance(-1.0F, 3600000.0F).noDrops());
+		super(Block.Properties.create(Material.PORTAL, MaterialColor.BLACK).doesNotBlockMovement().lightValue(6).hardnessAndResistance(-1.0F, 3600000.0F).noDrops());
 	}
 
 
@@ -36,14 +39,14 @@ public class WBPortalBlock extends ContainerBlock
 
 	public void onEntityCollision(BlockState blockState, World world, BlockPos position, Entity entity)
 	{
-//		if (!world.isRemote && 
-//			!entity.isPassenger() && 
-//			!entity.isBeingRidden() && 
-//			entity.isNonBoss() && 
-//			VoxelShapes.compare(VoxelShapes.create(entity.getBoundingBox().offset((double) (-position.getX()), (double) (-position.getY()), (double) (-position.getZ()))), blockState.getShape(world, position), IBooleanFunction.AND))
-//		{
-//			entity.changeDimension(world.dimension.getType() == WBDimension.worldblender() ? DimensionType.OVERWORLD : WBDimension.worldblender());
-//		}
+		if (!world.isRemote && 
+			!entity.isPassenger() && 
+			!entity.isBeingRidden() && 
+			entity.isNonBoss() && 
+			VoxelShapes.compare(VoxelShapes.create(entity.getBoundingBox().offset((double) (-position.getX()), (double) (-position.getY()), (double) (-position.getZ()))), blockState.getShape(world, position), IBooleanFunction.AND))
+		{
+			entity.changeDimension(world.dimension.getType() == WBDimension.worldblender() ? DimensionType.OVERWORLD : WBDimension.worldblender());
+		}
 	}
 
 
@@ -54,31 +57,18 @@ public class WBPortalBlock extends ContainerBlock
 	public void animateTick(BlockState blockState, World world, BlockPos position, Random random)
 	{
 		TileEntity tileentity = world.getTileEntity(position);
-		if (tileentity instanceof EndGatewayTileEntity)
+		if (tileentity instanceof WBPortalTileEntity)
 		{
-			int maxParticleCount = ((EndGatewayTileEntity) tileentity).getParticleAmount() * 3; //LOTS OF PARTICLES
-
-			for (int currentParticleCount = 0; currentParticleCount < maxParticleCount; ++currentParticleCount)
+			if (random.nextFloat() < 0.09f)
 			{
 				double xPos = (double) position.getX() + (double) random.nextFloat();
 				double yPos = (double) position.getY() + (double) random.nextFloat();
 				double zPos = (double) position.getZ() + (double) random.nextFloat();
-				double xVelocity = ((double) random.nextFloat() - 0.5D) * 0.5D;
-				double yVelocity = ((double) random.nextFloat() - 0.5D) * 0.5D;
-				double zVelocity = ((double) random.nextFloat() - 0.5D) * 0.5D;
-				int speedScale = random.nextInt(2) * 2 - 1;
-				if (random.nextBoolean())
-				{
-					zPos = (double) position.getZ() + 0.5D + 0.25D * (double) speedScale;
-					zVelocity = (double) (random.nextFloat() * 2.0F * (float) speedScale);
-				}
-				else
-				{
-					xPos = (double) position.getX() + 0.5D + 0.25D * (double) speedScale;
-					xVelocity = (double) (random.nextFloat() * 2.0F * (float) speedScale);
-				}
+				double xVelocity = ((double) random.nextFloat() - 0.5D) * 0.08D;
+				double yVelocity = ((double) random.nextFloat() - 0.5D) * 0.13D;
+				double zVelocity = ((double) random.nextFloat() - 0.5D) * 0.08D;
 
-				world.addParticle(ParticleTypes.DRAGON_BREATH, xPos, yPos, zPos, xVelocity, yVelocity, zVelocity);
+				world.addParticle(ParticleTypes.END_ROD, xPos, yPos, zPos, xVelocity, yVelocity, zVelocity);
 			}
 
 		}
--- src/main/java/net/telepathicgrunt/worldblender/blocks/WBPortalTileEntityRenderer.java
+++ src/main/java/net/telepathicgrunt/worldblender/blocks/WBPortalTileEntityRenderer.java
@@ -6,11 +6,8 @@ import java.util.stream.IntStream;
 
 import com.google.common.collect.ImmutableList;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 
-import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderState;
@@ -35,36 +32,36 @@ public class WBPortalTileEntityRenderer extends TileEntityRenderer<WBPortalTileE
 
 
 	@Override
-	public void render(WBPortalTileEntity tileEntity, float p_225616_2_, MatrixStack p_225616_3_, IRenderTypeBuffer renderBuffer, int p_225616_5_, int p_225616_6_)
+	public void render(WBPortalTileEntity tileEntity, float p_225616_2_, MatrixStack modelMatrix, IRenderTypeBuffer renderBuffer, int p_225616_5_, int p_225616_6_)
 	{
 		RANDOM.setSeed(31100L);
 		double distance = tileEntity.getPos().distanceSq(this.dispatcher.renderInfo.getProjectedView(), true);
 		int passes = this.getPasses(distance);
-		Matrix4f matrix4f = p_225616_3_.peek().getModel();
-		this.func_228883_a_(tileEntity, 0.15F, matrix4f, renderBuffer.getBuffer(WB_RENDER_TYPE.get(0)));
+		Matrix4f matrix4f = modelMatrix.peek().getModel();
+		this.drawColor(tileEntity, 0.15F, matrix4f, renderBuffer.getBuffer(WB_RENDER_TYPE.get(0)));
 
 		for (int currentPass = 1; currentPass < passes; ++currentPass)
 		{
-			this.func_228883_a_(tileEntity, 2.0F / (float) (18 - currentPass), matrix4f, renderBuffer.getBuffer(WB_RENDER_TYPE.get(currentPass)));
+			this.drawColor(tileEntity, 2.0F / (float) (18 - currentPass), matrix4f, renderBuffer.getBuffer(WB_RENDER_TYPE.get(currentPass)));
 		}
 	}
 
 
-	private void func_228883_a_(WBPortalTileEntity tileEntity, float p_228883_3_, Matrix4f matrix4f, IVertexBuilder vertexBuilder)
+	private void drawColor(WBPortalTileEntity tileEntity, float modifier, Matrix4f matrix4f, IVertexBuilder vertexBuilder)
 	{
-		float f = (RANDOM.nextFloat() * 0.5F + 0.8F) * p_228883_3_;
-		float f1 = (RANDOM.nextFloat() * 0.5F + 0.3F) * p_228883_3_;
-		float f2 = (RANDOM.nextFloat() * 0.5F + 0.1F) * p_228883_3_;
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, f, f1, f2, Direction.SOUTH);
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, f, f1, f2, Direction.NORTH);
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, f, f1, f2, Direction.EAST);
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F, f, f1, f2, Direction.WEST);
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, f, f1, f2, Direction.DOWN);
-		this.func_228884_a_(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, f, f1, f2, Direction.UP);
+		float red = (RANDOM.nextFloat() * 1.1F) * modifier;
+		float green = (RANDOM.nextFloat() * 2.1F) * modifier;
+		float blue = (RANDOM.nextFloat() * 0.8F) * modifier;
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, red, green, blue, Direction.SOUTH);
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, red, green, blue, Direction.NORTH);
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 0.0F, red, green, blue, Direction.EAST);
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 0.0F, 0.0F, 0.0F, 1.0F, 0.0F, 1.0F, 1.0F, 0.0F, red, green, blue, Direction.WEST);
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.0F, 1.0F, 1.0F, red, green, blue, Direction.DOWN);
+		this.setVertexColor(tileEntity, matrix4f, vertexBuilder, 0.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 0.0F, 0.0F, red, green, blue, Direction.UP);
 	}
 
 
-	private void func_228884_a_(WBPortalTileEntity tileEntity, Matrix4f matrix4f, IVertexBuilder vertexBuilder, float p_228884_4_, float p_228884_5_, float p_228884_6_, float p_228884_7_, float p_228884_8_, float p_228884_9_, float p_228884_10_, float p_228884_11_, float p_228884_12_, float p_228884_13_, float p_228884_14_, Direction direction)
+	private void setVertexColor(WBPortalTileEntity tileEntity, Matrix4f matrix4f, IVertexBuilder vertexBuilder, float p_228884_4_, float p_228884_5_, float p_228884_6_, float p_228884_7_, float p_228884_8_, float p_228884_9_, float p_228884_10_, float p_228884_11_, float p_228884_12_, float p_228884_13_, float p_228884_14_, Direction direction)
 	{
 		if (tileEntity.shouldRenderFace(direction))
 		{
@@ -73,7 +70,6 @@ public class WBPortalTileEntityRenderer extends TileEntityRenderer<WBPortalTileE
 			vertexBuilder.vertex(matrix4f, p_228884_5_, p_228884_7_, p_228884_10_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
 			vertexBuilder.vertex(matrix4f, p_228884_4_, p_228884_7_, p_228884_11_).color(p_228884_12_, p_228884_13_, p_228884_14_, 1.0F).endVertex();
 		}
-
 	}
 
 
@@ -116,8 +112,7 @@ public class WBPortalTileEntityRenderer extends TileEntityRenderer<WBPortalTileE
 	
 	//////////////////////////////////RENDER STATE STUFF//////////////////////////////////////////
 	
-	public static final ResourceLocation BACK_TEXTURE = new ResourceLocation("textures/environment/snow.png");
-	public static final ResourceLocation FRONT_TEXTURE = new ResourceLocation("textures/misc/enchanted_item_glint.png");
+	public static final ResourceLocation MAIN_TEXTURE = new ResourceLocation("textures/misc/enchanted_item_glint.png");
 	private static final Random RANDOM = new Random(31100L);
 	private static final List<RenderType> WB_RENDER_TYPE = IntStream.range(0, 16).mapToObj((index) ->
 	{
@@ -131,15 +126,15 @@ public class WBPortalTileEntityRenderer extends TileEntityRenderer<WBPortalTileE
 		RenderState.TextureState renderstate$texturestate;
 		if (p_228630_0_ <= 1)
 		{
-			renderstate$transparencystate = TRANSLUCENT_TRANSPARENCY;
-			renderstate$texturestate = new RenderState.TextureState(BACK_TEXTURE, false, false);
+			renderstate$transparencystate = RenderState.TRANSLUCENT_TRANSPARENCY;
+			renderstate$texturestate = new RenderState.TextureState(MAIN_TEXTURE, false, false);
 		}
 		else
 		{
-			renderstate$transparencystate = ADDITIVE_TRANSPARENCY;
-			renderstate$texturestate = new RenderState.TextureState(FRONT_TEXTURE, false, false);
+			renderstate$transparencystate = RenderState.ADDITIVE_TRANSPARENCY;
+			renderstate$texturestate = new RenderState.TextureState(MAIN_TEXTURE, false, false);
 		}
 
-		return RenderType.of("end_portal", DefaultVertexFormats.POSITION_COLOR, 7, 256, false, true, RenderType.State.builder().transparency(renderstate$transparencystate).texture(renderstate$texturestate).texturing(new RenderState.PortalTexturingState(p_228630_0_)).fog(BLACK_FOG).build(false));
+		return RenderType.of("end_portal", DefaultVertexFormats.POSITION_COLOR, 7, 256, false, true, RenderType.State.builder().transparency(renderstate$transparencystate).texture(renderstate$texturestate).texturing(new RenderState.PortalTexturingState(p_228630_0_)).fog(RenderState.BLACK_FOG).build(false));
 	}
 }
