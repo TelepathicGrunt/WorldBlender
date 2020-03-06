0d1c4c337a860f89b91551c10a88ae0a3a273de4
alex sarnese <alexthepokemon@Gmail.com>
2020-03-06 13:39:22 -0500

    fixed bug where dimension dungeon feature's weren't being added correctly

--- src/main/java/net/telepathicgrunt/worldblender/the_blender/dedicated_mod_support/DimDungeonsCompatibility.java
+++ src/main/java/net/telepathicgrunt/worldblender/the_blender/dedicated_mod_support/DimDungeonsCompatibility.java
@@ -6,6 +6,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.Placement;
@@ -26,15 +27,21 @@ public class DimDungeonsCompatibility
 			List<ConfiguredFeature<?, ?>> cflist = blendedBiome.getFeatures(GenerationStage.Decoration.SURFACE_STRUCTURES);
 			for (int i = cflist.size() - 1; i >= 0; i--)
 			{
+				DecoratedFeatureConfig decoratedFeatureConfig;
+				if(cflist.get(i).config instanceof DecoratedFeatureConfig)
+					decoratedFeatureConfig = (DecoratedFeatureConfig) cflist.get(i).config;
+				else 
+					continue;
+				
 				//only add our DD feature if the biome contains the actual mod's feature
-				if(cflist.get(i).feature.getRegistryName().equals(DD_BASIC_DUNGEON_RL) ||
-				   cflist.get(i).feature.getRegistryName().equals(DD_ADVANCED_DUNGEON_RL)) 
+				if(decoratedFeatureConfig.feature.feature.getRegistryName().equals(DD_BASIC_DUNGEON_RL) ||
+				   decoratedFeatureConfig.feature.feature.getRegistryName().equals(DD_ADVANCED_DUNGEON_RL)) 
 				{
 					//remove DD's dungeon since it wont spawn normally in our biome
 					blendedBiome.features.get(GenerationStage.Decoration.SURFACE_STRUCTURES).remove(cflist.get(i));
 					
 					//adds our DD dungeon feature if it isn't added yet
-					if(!blendedBiome.getFeatures(GenerationStage.Decoration.UNDERGROUND_STRUCTURES).stream().anyMatch(alreadyAddedFeature -> alreadyAddedFeature.feature.getRegistryName().equals(WBFeatures.DD_DUNGEON_FEATURE.getRegistryName()))) 
+					if(!blendedBiome.getFeatures(GenerationStage.Decoration.UNDERGROUND_STRUCTURES).stream().anyMatch(alreadyAddedFeature -> alreadyAddedFeature.config instanceof DecoratedFeatureConfig ? ((DecoratedFeatureConfig)alreadyAddedFeature.config).feature.feature.getRegistryName().equals(WBFeatures.DD_DUNGEON_FEATURE.getRegistryName()) : false)) 
 					{
 						
 						//add our dungeon instead
