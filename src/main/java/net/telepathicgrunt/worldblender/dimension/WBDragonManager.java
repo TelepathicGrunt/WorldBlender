e8bd7eb1d7a25fc2f805d32dff6514e4a650677c
alex sarnese <alexthepokemon@Gmail.com>
2020-03-08 23:36:13 -0400

    removed imports

--- src/main/java/net/telepathicgrunt/worldblender/dimension/WBDragonManager.java
+++ src/main/java/net/telepathicgrunt/worldblender/dimension/WBDragonManager.java
@@ -8,9 +8,6 @@ import java.util.function.Predicate;
 
 import javax.annotation.Nullable;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
